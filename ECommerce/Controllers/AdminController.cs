998999cc1e00ca85d8368f0d0911cd99eb056e28
Kerem Meydan <kerem.meydan34@gmail.com>
2023-05-26 00:56:21 +0300

    Add project files.

---
+++ ECommerce/Controllers/AdminController.cs
@@ -0,0 +1,12 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace ECommerceDemo.Controllers
+{
+	public class AdminController : Controller
+	{
+		public IActionResult Index()
+		{
+			return View();
+		}
+	}
+}
